9653b9e125995355c0097c1c76ecb4e792b444b0
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-21 20:20:03 -0400

    add basic css, fix html, and add js dependencies

--- code-quiz.js
+++ code-quiz.js
@@ -1,132 +1,64 @@
 
   //DEPENDENCIES
-var begin = document.querySelector(".begin");
+var beginQuiz = document.querySelector(".beginQuiz");
 var homepage = document.querySelector(".homepage");
 var main = document.querySelector("main");
-var questions = document.querySelector(".questions");
-var timeEl = document.querySelector(".timer");
-var displayAnswers = document.querySelector(".displayAnswers");
-var enterInitials = document.querySelector("#initialInput");
-var submit = document.querySelector("#submit");
+var questionBox = document.querySelector(".questions");
+var timerEl = document.querySelector(".timer");
+var displayAnswers = document.querySelector("#answer");
+var enterInitials = document.querySelector("#enter-initials");
+var submitInitials = document.querySelector("#submit");
 var warningMessage = document.querySelector("#warningMessage");
 var highscore = document.querySelector(".highscore");
 var highscoreList = document.querySelector(".highscoreList");
 var li = document.createElement("li");
 var clearHighscore = document.querySelector("#clearHighscore");
 var returnHome = document.querySelector("#returnHome");
-var highscoreButton = document.querySelector(".highscoreButtons");
+var highscoreButton = document.querySelector(".highscoreButton");
 
-// add questions
-var questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
+var timeRemaining = 60;
+var i = 0;
+var scoreHistory = [];
+
+var question1 = {
+    question: "1. What does HTML stand for?",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "High Tension Markup Language",
-      "Helpful Text Machine Learning",
-      
-    ]
-  },
-    {
-    numb: 2,
-    question: "What movie were the inventors of the internet most inspired by?",
-    answer: "Star Wars",
+    "1 - Hyper Text Preprocessor", 
+    "2 - Hyper Text Markup Language", 
+    "3 - High Tension Markup Language",
+    "4 - Helpful Text Machine Learning",
+    ],
+    correctAnswer: "2 - Hyper Text Markup Language",
+  };
+  
+  var question2 = {
+    question:
+      "2. What movie were the inventors of the internet most inspired by?",
     options: [
-      "iRobot",
-      "Star Wars",
-     "The Matrix",
-     "Inception",
-    ]
-  },
-    {
-    numb: 3,
-    question: "Arrays in Javascript can be used to store _____",
-    answer: "All of the above",
+      "1 - iRobot",
+      "2 - Star Wars",
+      "3 - The Matrix",
+      "4 - Inception",
+    ],
+    correctAnswer: "2 - Star Wars",
+  };
+
+  var question3 = {
+    question: "3. Arrays in Javascript can be used to store ______.",
     options: [
-      "Numbers and Strings",
-      "Other Arrays",
-      "Booleans",
-      "All of the above",
-    ]
-  },
+      "1 - Numbers and strings",
+      "2 - Other Arrays",
+      "3 - Booleans",
+      "4 - All of the above",
+    ],
+    correctAnswer: "4 - All of the above",
+  };
+ 
+  // Put questions into an Array
+  var questionDisplay = [
+    question1,
+    question2,
+    question3,
   ];
-  // FUNCTIONS
-// Once begin is pressed, timer also begins
-function beginQuiz() {
-    var timeInterval = setInterval(function () {
-      timeLeft--;
-      timeEl.textContent = timeLeft + "s left";
-      if (timeLeft < 0 || i === questionDisplay.length) {
-        clearInterval(timeInterval);
-        timeEl.textContent = "";
-        InputYourName();
-      }
-    }, 1000);
-    highscore.style.display = "none";
-    homepage.style.display = "none";
-    questionDisplay.style.display = "";
-    quizQuestions();
-  }
-  // Check answers once questions are complete
-  function quizQuestions() {
-    if (i < questionDisplay.length) {
-      generateQuestion();
-      checkAnswer();
-    } else {
-      return;
-    }
-  }
-
-  // Place questions in array to show up on page
-var questionDisplay = [ 1, 2, 3];
-
-// Loading questions and options
-function generateQuestion() {
-    questions.children[0].textContent = questionDisplay[i].question;
-    questions.children[1].children[0].textContent =
-    questionDisplay[i].option["Hyper Text Markup Language"];
-    questions.children[2].children[0].textContent =
-    questionDisplay[i].option["Star Wars"];
-    questions.children[3].children[0].textContent =
-    questionDisplay[i].option["All of the above"];
-  }
-  // Check if answer is correct
-    function checkAnswer() {
-    questions.addEventListener("click", function (event) {
-      event.stopImmediatePropagation();
-      if (
-        event.target.matches("button") &&
-        questionDisplay[i].correctAnswer === event.target.textContent
-      ) {
-        i++;
-        quizQuestions();
-        displayAnswers.style.display = "";
-        displayAnswers.children[1].textContent = "Correct Answer!";
-        quizQuestions();
-      } else if (
-        event.target.matches("button") &&
-        questionDisplay[i].correctAnswer !== event.target.textContent
-      ) {
-        i++;
-        quizQuestions();
-        displayAnswers.style.display = "";
-        displayAnswers.children[1].textContent = "Incorrect Anwswer!";
-        displayAnswers();
-      }
-    });
-  }
-  // Reveal correct answer
-function displayAnswers() {
-    var answerInterval = setInterval(function () {
-      clearInterval(answerInterval);
-      displayAnswers.children[1].textContent = "";
-      displayAnswers.style.display = "none";
-    });
-  }
-
-  // USER INTERACTION
-
-begin.addEventListener("click", beginQuiz);
+  
+  
